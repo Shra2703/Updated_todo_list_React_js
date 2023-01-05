081a647606e90da364146d13443d42cabf2102e2
Shraddha Goyal <shraddhagoyal800@gmail.com>
2023-01-05 17:55:48 +0530

    update part is completed

--- src/component/Todo.jsx
+++ src/component/Todo.jsx
@@ -5,19 +5,50 @@ import todo from "../images/todo.png";
 const Todo = () => {
   const [inputData, setInputData] = useState("");
   const [items, setItems] = useState([]);
+  const [toggle, setToggle] = useState(true);
+  const [isEdit, setIsEdit] = useState(null);
 
   const addItems = () => {
-    if (!inputData) alert("Sorry!! Can't add");
-    else {
-      setItems([...items, inputData]);
-      setInputData(" ");
+    if (!inputData || inputData === " ") {
+      alert("Sorry!! Can't add");
+    } else if (inputData && !toggle) {
+      setItems(
+        items.map((ele) => {
+          if (ele.id === isEdit) {
+            return {
+              ...ele,
+              name: inputData,
+            };
+          }
+          return ele;
+        })
+      );
+      setToggle(true);
+      setInputData("");
+      setIsEdit(null);
+    } else {
+      const allInputData = {
+        id: new Date().getTime().toString(),
+        name: inputData,
+      };
+      setItems([...items, allInputData]);
+      setInputData("");
     }
   };
-  const deleteItems = (id) => {
-    const updatedItems = items.filter((ele, index) =>{
-      return id !== index;
+  const deleteItems = (index) => {
+    const updatedItems = items.filter((ele) => {
+      return ele.id !== index;
     });
     setItems(updatedItems);
+  };
+  const editItems = (index) => {
+    let newEditItem = items.find((ele) => {
+      return ele.id === index;
+    });
+    setToggle(false);
+    setInputData(newEditItem.name);
+    setIsEdit(index);
+    console.log(newEditItem);
   };
 
   return (
@@ -35,24 +66,41 @@ const Todo = () => {
               value={inputData}
               onChange={(e) => setInputData(e.target.value)}
             />
-            <i
-              className="fa fa-plus add-btn"
-              title="Add button"
-              onClick={addItems}
-            ></i>
+            {toggle ? (
+              <i
+                className="fa fa-plus add-btn"
+                title="Add button"
+                onClick={addItems}
+              ></i>
+            ) : (
+              <i
+                className="fa fa-edit add-btn"
+                title="Update button"
+                onClick={addItems}
+              ></i>
+            )}
           </div>
           <div className="showItems">
-            {items.map((ele, index) => {
+            {items.map((ele) => {
               return (
-                <div className="eachItem" key={index}>
-                  <h3>{ele}</h3>
-                  <i
-                    className="fa fa-trash-alt add-btn"
-                    title="Delete button"
-                    onClick={() => {
-                      deleteItems(index);
-                    }}
-                  ></i>
+                <div className="eachItem" key={ele.id}>
+                  <h3>{ele.name}</h3>
+                  <div className="todo-btn">
+                    <i
+                      className="fa fa-edit add-btn"
+                      title="Edit button"
+                      onClick={() => {
+                        editItems(ele.id);
+                      }}
+                    ></i>
+                    <i
+                      className="fa fa-trash-alt add-btn"
+                      title="Delete button"
+                      onClick={() => {
+                        deleteItems(ele.id);
+                      }}
+                    ></i>
+                  </div>
                 </div>
               );
             })}
